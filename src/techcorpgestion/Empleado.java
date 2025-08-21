d87001fb061e9d94e3c4f0f91e702efdb3f4cf3b
Paloma2004ddg <161170409+Paloma2004ddg@users.noreply.github.com>
2025-08-21 08:21:57 -0300

    1ER

---
+++ src/techcorpgestion/Empleado.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package techcorpgestion;
+
+public  abstract class Empleado {
+    protected String nombre;
+    protected String idEmpleado;
+    protected double salario;
+    //constructor
+    
+   public Empleado(String nombre, String idEmpleado, double salario){
+       this.nombre = nombre;
+       this.idEmpleado = idEmpleado;
+       this.salario = salario;
+   }
+    //getters y setters
+   public String getnombre(){
+       return nombre;
+   }
+   public void setnombre(String nombre){
+       this.nombre = nombre;
+   }
+   public String getidEmpleado(){
+       return idEmpleado;
+   }
+   public void setidEmpleado(String idEmpleado){
+       this.idEmpleado = idEmpleado;
+   }
+   public double getsalario(){
+       return salario;
+   }
+   public void setsalario(double salario){
+       this.salario = salario;
+   }
+   //metodo abstracto(polimorfismo)
+   public abstract void trabajar();{
+    
+}
+   //metodo comun
+   public void mostrarInfo(){
+       System.out.println("ID: "+ idEmpleado + " | Nombre: "+ nombre + "| Salario: $" + salario);
+   }
+}
